8e6fd9139203110e091c9022b2c38b80106fd58a
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-06-07 11:39:54 +0300

    Update engine.js

--- src/engine.js
+++ src/engine.js
@@ -7,36 +7,3 @@ function gameLoop(game) {
 
   window.requestAnimationFrame(gameLoop.bind(null, game));
 }
-
-function moveWizard(game) {
-  const { stateOfWizard } = game;
-  const { wizardElement } = game;
-
-  if (game.keys.KeyD) {
-    if (stateOfWizard.left < game.screen.offsetWidth) {
-      stateOfWizard.left += stateOfWizard.speed;
-    }
-  }
-  if (game.keys.KeyA) {
-    if (stateOfWizard.left > 10) {
-      stateOfWizard.left -= stateOfWizard.speed;
-    }
-  }
-  if (game.keys.KeyW) {
-    if (stateOfWizard.top > 10) {
-      stateOfWizard.top -= stateOfWizard.speed;
-    }
-  }
-  if (game.keys.KeyS) {
-    if (stateOfWizard.top < game.screen.offsetHeight - 5) {
-      stateOfWizard.top += stateOfWizard.speed;
-    }
-  }
-
-  wizardElement.style.left =
-    Math.floor(Math.max(stateOfWizard.left - stateOfWizard.width, 0) - 5) +
-    "px";
-
-  wizardElement.style.top =
-    Math.floor(Math.max(stateOfWizard.top - stateOfWizard.height, 0)) + "px";
-}
